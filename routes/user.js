eadbbe6a9613eb221ec0b027f281b7c257313b4c
RocTanweer <mohammadtanweer769@gmail.com>
2021-11-07 21:19:34 +0530

    ADD: added routes for logout, profile details and profile edit

--- routes/user.js
+++ routes/user.js
@@ -2,17 +2,20 @@ import express from "express";
 import {
   userRegistration,
   userLogin,
+  userLogout,
   getUserProfile,
   patchUserProfile,
   handleEveryrouteElse,
 } from "../controllers/user.js";
+import { checkForAuthorizationToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/register", userRegistration);
 router.post("/login", userLogin);
-router.get("/profile/:userId", getUserProfile);
-router.patch("/profile/:userId", patchUserProfile);
+router.delete("/logout", userLogout);
+router.get("/profile/:userId", checkForAuthorizationToken, getUserProfile);
+router.patch("/profile/:userId", checkForAuthorizationToken, patchUserProfile);
 router.get("*", handleEveryrouteElse);
 
 export default router;
